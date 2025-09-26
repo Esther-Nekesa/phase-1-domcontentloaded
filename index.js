c1d62b87ed475bd656980b9a2d155a0b1515619f
Esther nekesa <esthernekesasi32@gmail.com>
2025-09-26 23:04:57 +0300

    add my DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Wait for the DOM to fully load
+document.addEventListener("DOMContentLoaded", function () {
+  // Target the <p> with id="text"
+  const paragraph = document.getElementById("text");
+  // Change its text content
+  paragraph.textContent = "This is really cool!";
+});
